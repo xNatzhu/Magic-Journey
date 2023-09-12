3bbf094fad6260d180d0fbd9ded8ab0bcaf9a6bd
xNatzhu <agustinnahuelsaravia@gmail.com>
2023-09-12 00:25:44 -0300

    Actualizacion de routing de user - SignIn

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import bcryptjs from "bcryptjs"
 
 export const signup = async(req, res)=>{
     try {
+
         const {name, country, email, password, img} = req.body
         const hashPassword = bcryptjs.hashSync(password, 12);
         const createUser = userModel.create({
@@ -13,9 +14,48 @@ export const signup = async(req, res)=>{
             img,
             password: hashPassword,
         })
-        res.status(200).json(createUser);
+        res.status(201).json({
+            status:201,
+            msg: "La cuenta se creo correctamente",
+            response:createUser
+        });
+
+
     } catch (error) {
         res.status(500).json({ msg: "Hubo un problema al crear la cuenta"});
         console.log(error);
     }
 }
+
+export const signIn = async (req, res)=>{
+    try {
+        const {email, password } = req.body
+
+        const findUser = await userModel.findOne({email})
+        console.log("Probando",findUser);
+        
+        if(!findUser){
+            return res.status(400).json({msg:"Dato ingresado incorrecto"})
+        }
+
+        const passCompare = await bcryptjs.compare(password, findUser.password); 
+
+        if(!passCompare){
+            return res.status(400).json({msg:"Dato ingresado incorrecto"})
+        }
+
+        console.log("comparacion", passCompare);
+        return res.send("Ingreso correctamente")
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Hubo un problema al ingresar a la cuenta"});
+        
+    }
+}
+
+
+
+
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import {validator} from "../middlewares/validator.js"
 import signupSchema from "../joi_Schemas/sign_up.js";
-import { signup } from "../controllers/user.controllers.js";
+import { signIn, signup } from "../controllers/user.controllers.js";
 const router = Router();
 
 router.post("/signup",validator(signupSchema), signup)
+router.post("/signin", signIn)
 
 
 
